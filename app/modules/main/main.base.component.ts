ef5761b4c37dfe7a098034a8d142dcdc6551a737
Anton Makridin <a.i.makridin@yandex.ru>
2017-09-25 23:58:36 +0300

    jquery added, base methods for disp figure

--- app/modules/main/main.base.component.ts
+++ app/modules/main/main.base.component.ts
@@ -1,7 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
 	templateUrl: 'app/templates/main.html',
 	styleUrls: ['app/templates/main.css']
 })
-export class MainBaseComponent {}
+
+export class MainBaseComponent {
+
+	public ngOnInit(): void {
+		this.setFigure1();
+	}
+
+	public setFigure1(): void {
+		//стартовая позиция, зададим клеткам ид сущности и тип фигуры
+		$('.main').find('td').eq(0).css('background','#000').addClass('figure-1').attr('type','1');
+		$('.main').find('td').eq(1).css('background','#000').addClass('figure-1').attr('type','1');
+		$('.main').find('td').eq(2).css('background','#000').addClass('figure-1').attr('type','1');
+		$('.main').find('td').eq(3).css('background','#000').addClass('figure-1').attr('type','1');
+		$('.main').find('td').eq(4).css('background','#000').addClass('figure-1').attr('type','1');
+	}
+}
