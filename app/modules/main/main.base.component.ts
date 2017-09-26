3377c7902329e28b92bdfdb5cf61869697ae909a
Anton Makridin <a.i.makridin@yandex.ru>
2017-09-26 19:38:44 +0300

    figures F,I added, beforeMove check redesign

--- app/modules/main/main.base.component.ts
+++ app/modules/main/main.base.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface ISettings {
+	maxRows: number;
+	maxColumns: number;
+}
+
 @Component({
 	templateUrl: 'app/templates/main.html',
 	styleUrls: ['app/templates/main.css']
@@ -8,69 +13,108 @@ import { Component, OnInit } from '@angular/core';
 export class MainBaseComponent {
 
 	private curFigureActiveId: string = null;
-	private settings: any = {
-		'maxRows': 20,
+	private settings: ISettings = {
+		'maxRows': 25,
 		'maxColumns': 12
 	}
 	private move: any = null;
 
 	public ngOnInit(): void {
 		window.scope = this;
-		this.setFigure1();
-		this.move = setInterval(() => {
-			this.moveFigure();
-		},500);
+		//this.createPlayField();
+		this.createFigureF();
+		//this.createFigureI();
 	}
 
-	public setFigure1(): void {
+	// public createPlayField(): void {
+	// 	let elTbody: Element = document.querySelector('.field');
+	// 	for(let j: number = 0; j < this.settings.maxColumns; j++){
+	// 		let elTr: Element = document.createElement('tr');			
+	// 		for(let i: number = 0; i < this.settings.maxColumns; i++){
+	// 			let elTd: Element = document.createElement('td');
+	// 			elTd.classList.add('block');
+	// 			elTr.appendChild(elTd);
+	// 		}
+	// 		elTbody.appendChild(elTr);
+	// 	}	
+	// }
+
+	public createFigureI(): void {
 		//создадим уникальный id
 		let hash: string = chance.hash({length: 8});
 		//объявим фигуру активной
 		this.curFigureActiveId = hash;
 		//стартовая позиция, зададим клеткам ид сущности, тип фигуры и номер строки
-		$('.block').eq(0).addClass(`figure_block figure_${hash}`).data('type',1).data('row',1);
-		$('.block').eq(1).addClass(`figure_block figure_${hash}`).data('type',1).data('row',1);
-		$('.block').eq(2).addClass(`figure_block figure_${hash}`).data('type',1).data('row',1);
-		$('.block').eq(3).addClass(`figure_block figure_${hash}`).data('type',1).data('row',1);
-		$('.block').eq(4).addClass(`figure_block figure_${hash}`).data('type',1).data('row',1);		
+		$('.block').eq(0).addClass(`figure_block figure_${hash}`).data('type','i').data('row',1);
+		$('.block').eq(1).addClass(`figure_block figure_${hash}`).data('type','i').data('row',1);
+		$('.block').eq(2).addClass(`figure_block figure_${hash}`).data('type','i').data('row',1);
+		$('.block').eq(3).addClass(`figure_block figure_${hash}`).data('type','i').data('row',1);
+		$('.block').eq(4).addClass(`figure_block figure_${hash}`).data('type','i').data('row',1);
+
+		this.move = setInterval(() => {
+			this.moveFigure();
+		},250);
+	}
+
+	public createFigureF(): void {
+		//создадим уникальный id
+		let hash: string = chance.hash({length: 8});
+		//объявим фигуру активной
+		this.curFigureActiveId = hash;
+		//стартовая позиция, зададим клеткам ид сущности, тип фигуры и номер строки
+		$('.block').eq(1).addClass(`figure_block figure_${hash}`).data('type','f').data('row',1);
+		$('.block').eq(2).addClass(`figure_block figure_${hash}`).data('type','f').data('row',1);
+		$('.block').eq(12).addClass(`figure_block figure_${hash}`).data('type','f').data('row',2);
+		$('.block').eq(13).addClass(`figure_block figure_${hash}`).data('type','f').data('row',2);
+		$('.block').eq(25).addClass(`figure_block figure_${hash}`).data('type','f').data('row',3);
+
+		this.move = setInterval(() => {
+			this.moveFigure();
+		},250);
 	}
 
 	public moveFigure(): void {
 		console.info('move');
 		//сохраним тип фигуры
-		let figureType: number = $(`.figure_${this.curFigureActiveId}`).data('type');		
+		let figureType: string = $(`.figure_${this.curFigureActiveId}`).data('type');		
 		;
-
-		if(figureType === 1){
+		//проверка можно ли сделать движение по всем частям фигуры
+		for(let i:number = 0; i < 5; i++){
+			let curBlock: JQuery = $(`.figure_${this.curFigureActiveId}`).eq(4 - i);
+			let row: number = curBlock.data('row');
+			let newBlock: JQuery = $('.block').eq((curBlock.index() + (this.settings.maxColumns * row)));
+			if((!newBlock.hasClass(`figure_${this.curFigureActiveId}`) && newBlock.hasClass('figure_block')) || row === this.settings.maxRows){
+				clearInterval(this.move);
+				this.move = null;
+				return;				
+			}
+		}
+		//выполним перерисовку
+		if(figureType === 'i'){
 			//'передвинем' фигуру по частям по очереди начиная с конца
 			for(let i:number = 0; i < 5; i++){
-
-				//определим следующий квадрат под закраску под фигуру
 				let curBlock: JQuery = $(`.figure_${this.curFigureActiveId}`).eq(4 - i);
 				let row: number = curBlock.data('row');
-				let newBlock: JQuery = $('.block').eq((curBlock.index() + (12 * row)));
-
-				//определим используется ли он и не конец ли поля
-				if((!newBlock.hasClass('figure_block')) && row < this.settings.maxRows){
-					//очистим текущий квадрат и уберем класс
-					curBlock.removeClass(`figure_block figure_${this.curFigureActiveId}`);
-					//покрасим новый квадрат и добавим класс
-					newBlock.addClass(`figure_block figure_${this.curFigureActiveId}`).data('type',1).data('row',(row + 1));
-					
-				}
-				else {
-					clearInterval(this.move);
-					this.move = null;
-					return;
-				}
-
-				
+				let newBlock: JQuery = $('.block').eq((curBlock.index() + (this.settings.maxColumns * row)));
+				//очистим текущий квадрат и уберем класс
+				curBlock.removeClass(`figure_block figure_${this.curFigureActiveId}`);
+				//покрасим новый квадрат и добавим класс
+				newBlock.addClass(`figure_block figure_${this.curFigureActiveId}`).data('type','i').data('row',(row + 1));	
 			}
-
+		}
+		else if(figureType === 'f'){
+			//'передвинем' фигуру по частям по очереди начиная с конца
+			for(let i:number = 0; i < 5; i++){
+				let curBlock: JQuery = $(`.figure_${this.curFigureActiveId}`).eq(4 - i);
+				let row: number = curBlock.data('row');
+				let newBlock: JQuery = $('.block').eq((curBlock.index() + (this.settings.maxColumns * row)));
+				//очистим текущий квадрат и уберем класс
+				curBlock.removeClass(`figure_block figure_${this.curFigureActiveId}`);
+				//покрасим новый квадрат и добавим класс
+				newBlock.addClass(`figure_block figure_${this.curFigureActiveId}`).data('type','f').data('row',(row + 1));	
+			}
 		}
 		else {}
-
-
 	}
 }
 
