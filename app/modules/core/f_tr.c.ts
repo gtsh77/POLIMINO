a65d7f67fcb22ca3ee0df5d7ea1eeb57ea625d76
Anton Makridin <a.i.makridin@yandex.ru>
2017-09-30 20:12:05 +0300

    rotate mech improvements

--- app/modules/core/f_tr.c.ts
+++ app/modules/core/f_tr.c.ts
@@ -14,28 +14,35 @@ interface rebuild_args {
 export class f_tr {
 	constructor(private scope: s_gl){}
 	public rebuild(data: rebuild_args[]): void {
+		let degree: any = $(`.figure_${this.scope.curFigureActiveId}[center=true]`).attr('rotation') || '0';
 		//полная проверка перед перерисовкой первого блока
 		for(let i: number = 0; i < data.length; i++){
 			let curBlock: JQuery = $(`.figure_${this.scope.curFigureActiveId}`).eq(data[i].id);
 			let column: number = curBlock.data('column');
 			let row: number = curBlock.data('row') - 1;
 			let newBlock: JQuery = $('.block').eq(curBlock.index() + (this.scope.settings.maxColumns * row) + data[i].id_diff);
-			if((!newBlock.hasClass(`figure_${this.scope.curFigureActiveId}`) && newBlock.hasClass('figure_block'))) return;
+			if((!newBlock.hasClass(`figure_${this.scope.curFigureActiveId}`) && newBlock.hasClass('figure_block'))){
+				console.warn('cant_rotate');
+				return;
+			}
 		}
 		for(let j: number = 0; j < data.length; j++){
 			let curBlock: JQuery = $(`.figure_${this.scope.curFigureActiveId}`).eq(data[j].id);
 			let column: number = curBlock.data('column');
 			let row: number = curBlock.data('row') - 1;
 			let newBlock: JQuery = $('.block').eq(curBlock.index() + (this.scope.settings.maxColumns * row) + data[j].id_diff);
+			console.log(newBlock[0]);
 			curBlock.removeClass(`figure_block figure_${this.scope.curFigureActiveId}`);
 			//покрасим новый квадрат и добавим класс
 			newBlock.addClass(`figure_block figure_${this.scope.curFigureActiveId}`).data('type',data[j].f_type).data('column',(column + data[j].col_diff)).data('row',row + data[j].row_diff + 1);
 		}
+		//установим инфу по ротации
+		$(`.figure_${this.scope.curFigureActiveId}[center=true]`).attr('rotation',this.getNewRVal(degree));
+		console.info('rotated');
 	
 	}
 
 	public rotate(): void {
-		console.info('rotate');
 		//сохраним тип фигуры
 		let figureType: string = $(`.figure_${this.scope.curFigureActiveId}`).data('type');
 		//сохраним градус поворота
@@ -63,7 +70,7 @@ export class f_tr {
 						id: 3,
 						id_diff: this.scope.settings.maxColumns,
 						col_diff: 0,
-						row_diff: 1
+						row_diff: +1
 					}
 				]);
 			}
@@ -198,92 +205,90 @@ export class f_tr {
 				]);				
 			}
 			else {}
-		// }
-		// else if(figureType === 'b'){
-		// 	if(degree === '0'){
-		// 		this.rebuild(figureType,0,(this.scope.settings.maxColumns * -2) + 2,2,-2);
-		// 		this.rebuild(figureType,1,(this.scope.settings.maxColumns * -1) + 1,1,-1);
-		// 		this.rebuild(figureType,3,this.scope.settings.maxColumns -1,-1,1);
-		// 		this.rebuild(figureType,3,(this.scope.settings.maxColumns *2) -2,-2,2);
-		// 	}
-		// 	else if(degree === '90'){
-		// 		this.rebuild(figureType,0,(this.scope.settings.maxColumns * 2) - 2,-2,2);
-		// 		this.rebuild(figureType,0,this.scope.settings.maxColumns - 1,-1,1);
-		// 		this.rebuild(figureType,3,(this.scope.settings.maxColumns * -1) +1,1,-1);
-		// 		this.rebuild(figureType,4,(this.scope.settings.maxColumns * -2) +2,2,-2);
-		// 	}
-		// 	if(degree === '180'){
-		// 		this.rebuild(figureType,0,(this.scope.settings.maxColumns * -2) + 2,2,-2);
-		// 		this.rebuild(figureType,1,(this.scope.settings.maxColumns * -1) + 1,1,-1);
-		// 		this.rebuild(figureType,3,this.scope.settings.maxColumns -1,-1,1);
-		// 		this.rebuild(figureType,3,(this.scope.settings.maxColumns *2) -2,-2,2);
-		// 	}
-		// 	else if(degree === '270'){
-		// 		this.rebuild(figureType,0,(this.scope.settings.maxColumns * 2) - 2,-2,2);
-		// 		this.rebuild(figureType,0,this.scope.settings.maxColumns - 1,-1,1);
-		// 		this.rebuild(figureType,3,(this.scope.settings.maxColumns * -1) +1,1,-1);
-		// 		this.rebuild(figureType,4,(this.scope.settings.maxColumns * -2) +2,2,-2);
-		// 	}
-		// 	else {}
-		// }
-		// else if(figureType === 'c'){
-		// 	if(degree === '0'){
-		// 		this.rebuild(figureType,0,(this.scope.settings.maxColumns * -1) + 1,+1,-1);
-		// 		this.rebuild(figureType,2,(this.scope.settings.maxColumns * 1) - 1,-1,+1);
-		// 		this.rebuild(figureType,2,(this.scope.settings.maxColumns * 2) -2,-2,+2);
-		// 		this.rebuild(figureType,3,(this.scope.settings.maxColumns * 1) -3,-3,+1);
-		// 	}
-		// 	else if(degree === '90'){
-		// 		this.rebuild(figureType,0,(this.scope.settings.maxColumns * 1) + 1,+1,+1);
-		// 		this.rebuild(figureType,2,(this.scope.settings.maxColumns * -1) - 1,-1,-1);
-		// 		this.rebuild(figureType,4,(this.scope.settings.maxColumns * -2) -2,-2,-2);
-		// 		this.rebuild(figureType,4,(this.scope.settings.maxColumns * -3) -1,-1,-3);
-		// 	}
-		// 	else if(degree === '180'){
-		// 		this.rebuild(figureType,0,(this.scope.settings.maxColumns * -1) + 3,+3,-1);
-		// 		this.rebuild(figureType,1,(this.scope.settings.maxColumns * -2) + 2,+2,-2);
-		// 		this.rebuild(figureType,2,(this.scope.settings.maxColumns * -1) +1,+1,-1);
-		// 		this.rebuild(figureType,4,(this.scope.settings.maxColumns * 1) -1,-1,+1);
-		// 	}
-		// 	else if(degree === '270'){
-		// 		this.rebuild(figureType,0,(this.scope.settings.maxColumns * 3) + 2,+2,+3);
-		// 		this.rebuild(figureType,0,(this.scope.settings.maxColumns * 2) + 1,+1,+2);
-		// 		this.rebuild(figureType,0,(this.scope.settings.maxColumns * 1) + 1,+1,+1);
-		// 		this.rebuild(figureType,3,(this.scope.settings.maxColumns * -1) -1,-1,-1);
-		// 	}
-		// 	else {}
-		// }
-		// else if(figureType === 'c2'){
-		// 	if(degree === '0'){
-		// 		this.rebuild(figureType,0,(this.scope.settings.maxColumns * -2) + 2,+2,-2);
-		// 		this.rebuild(figureType,1,(this.scope.settings.maxColumns * -1) +1,+1,-1);
-		// 		this.rebuild(figureType,3,(this.scope.settings.maxColumns * 1) -1,-1,+1);
-		// 		this.rebuild(figureType,3,(this.scope.settings.maxColumns * -3) +1,+1,-3);
-		// 	}
-		// 	else if(degree === '90'){
-		// 		this.rebuild(figureType,0,(this.scope.settings.maxColumns * 1) + 3,+3,+1);
-		// 		this.rebuild(figureType,0,(this.scope.settings.maxColumns * 2) +2,+2,+2);
-		// 		this.rebuild(figureType,0,(this.scope.settings.maxColumns * 1) +1,+1,+1);
-		// 		this.rebuild(figureType,4,(this.scope.settings.maxColumns * -1) -1,-1,-1);
-		// 	}
-		// 	else if(degree === '180'){
-		// 		this.rebuild(figureType,1,(this.scope.settings.maxColumns * -1) + 1,+1,-1);
-		// 		this.rebuild(figureType,3,(this.scope.settings.maxColumns * 1) - 1,-1,+1);
-		// 		this.rebuild(figureType,3,(this.scope.settings.maxColumns * 2) -2,-2,+2);
-		// 		this.rebuild(figureType,1,(this.scope.settings.maxColumns * 3) -1,-1,+3);
-		// 	}
-		// 	else if(degree === '270'){
-		// 		this.rebuild(figureType,0,(this.scope.settings.maxColumns * 1) + 1,+1,+1);
-		// 		this.rebuild(figureType,2,(this.scope.settings.maxColumns * -1) - 1,-1,-1);
-		// 		this.rebuild(figureType,3,(this.scope.settings.maxColumns * -2) - 2,-2,-2);
-		// 		this.rebuild(figureType,4,(this.scope.settings.maxColumns * -1) -3,-3,-1);
-		// 	}
-		// 	else {}
-		// }
-		// else if(figureType === 'l') return;
-		// else {}		
-		//установим инфу по ротации
-		$(`.figure_${this.scope.curFigureActiveId}[center=true]`).attr('rotation',this.getNewRVal(degree));
+		}
+		else if(figureType === 'b'){
+			if(degree === '0'){
+				this.rebuild(figureType,0,(this.scope.settings.maxColumns * -2) + 2,2,-2);
+				this.rebuild(figureType,1,(this.scope.settings.maxColumns * -1) + 1,1,-1);
+				this.rebuild(figureType,3,this.scope.settings.maxColumns -1,-1,1);
+				this.rebuild(figureType,3,(this.scope.settings.maxColumns *2) -2,-2,2);
+			}
+			else if(degree === '90'){
+				this.rebuild(figureType,0,(this.scope.settings.maxColumns * 2) - 2,-2,2);
+				this.rebuild(figureType,0,this.scope.settings.maxColumns - 1,-1,1);
+				this.rebuild(figureType,3,(this.scope.settings.maxColumns * -1) +1,1,-1);
+				this.rebuild(figureType,4,(this.scope.settings.maxColumns * -2) +2,2,-2);
+			}
+			if(degree === '180'){
+				this.rebuild(figureType,0,(this.scope.settings.maxColumns * -2) + 2,2,-2);
+				this.rebuild(figureType,1,(this.scope.settings.maxColumns * -1) + 1,1,-1);
+				this.rebuild(figureType,3,this.scope.settings.maxColumns -1,-1,1);
+				this.rebuild(figureType,3,(this.scope.settings.maxColumns *2) -2,-2,2);
+			}
+			else if(degree === '270'){
+				this.rebuild(figureType,0,(this.scope.settings.maxColumns * 2) - 2,-2,2);
+				this.rebuild(figureType,0,this.scope.settings.maxColumns - 1,-1,1);
+				this.rebuild(figureType,3,(this.scope.settings.maxColumns * -1) +1,1,-1);
+				this.rebuild(figureType,4,(this.scope.settings.maxColumns * -2) +2,2,-2);
+			}
+			else {}
+		}
+		else if(figureType === 'c'){
+			if(degree === '0'){
+				this.rebuild(figureType,0,(this.scope.settings.maxColumns * -1) + 1,+1,-1);
+				this.rebuild(figureType,2,(this.scope.settings.maxColumns * 1) - 1,-1,+1);
+				this.rebuild(figureType,2,(this.scope.settings.maxColumns * 2) -2,-2,+2);
+				this.rebuild(figureType,3,(this.scope.settings.maxColumns * 1) -3,-3,+1);
+			}
+			else if(degree === '90'){
+				this.rebuild(figureType,0,(this.scope.settings.maxColumns * 1) + 1,+1,+1);
+				this.rebuild(figureType,2,(this.scope.settings.maxColumns * -1) - 1,-1,-1);
+				this.rebuild(figureType,4,(this.scope.settings.maxColumns * -2) -2,-2,-2);
+				this.rebuild(figureType,4,(this.scope.settings.maxColumns * -3) -1,-1,-3);
+			}
+			else if(degree === '180'){
+				this.rebuild(figureType,0,(this.scope.settings.maxColumns * -1) + 3,+3,-1);
+				this.rebuild(figureType,1,(this.scope.settings.maxColumns * -2) + 2,+2,-2);
+				this.rebuild(figureType,2,(this.scope.settings.maxColumns * -1) +1,+1,-1);
+				this.rebuild(figureType,4,(this.scope.settings.maxColumns * 1) -1,-1,+1);
+			}
+			else if(degree === '270'){
+				this.rebuild(figureType,0,(this.scope.settings.maxColumns * 3) + 2,+2,+3);
+				this.rebuild(figureType,0,(this.scope.settings.maxColumns * 2) + 1,+1,+2);
+				this.rebuild(figureType,0,(this.scope.settings.maxColumns * 1) + 1,+1,+1);
+				this.rebuild(figureType,3,(this.scope.settings.maxColumns * -1) -1,-1,-1);
+			}
+			else {}
+		}
+		else if(figureType === 'c2'){
+			if(degree === '0'){
+				this.rebuild(figureType,0,(this.scope.settings.maxColumns * -2) + 2,+2,-2);
+				this.rebuild(figureType,1,(this.scope.settings.maxColumns * -1) +1,+1,-1);
+				this.rebuild(figureType,3,(this.scope.settings.maxColumns * 1) -1,-1,+1);
+				this.rebuild(figureType,3,(this.scope.settings.maxColumns * -3) +1,+1,-3);
+			}
+			else if(degree === '90'){
+				this.rebuild(figureType,0,(this.scope.settings.maxColumns * 1) + 3,+3,+1);
+				this.rebuild(figureType,0,(this.scope.settings.maxColumns * 2) +2,+2,+2);
+				this.rebuild(figureType,0,(this.scope.settings.maxColumns * 1) +1,+1,+1);
+				this.rebuild(figureType,4,(this.scope.settings.maxColumns * -1) -1,-1,-1);
+			}
+			else if(degree === '180'){
+				this.rebuild(figureType,1,(this.scope.settings.maxColumns * -1) + 1,+1,-1);
+				this.rebuild(figureType,3,(this.scope.settings.maxColumns * 1) - 1,-1,+1);
+				this.rebuild(figureType,3,(this.scope.settings.maxColumns * 2) -2,-2,+2);
+				this.rebuild(figureType,1,(this.scope.settings.maxColumns * 3) -1,-1,+3);
+			}
+			else if(degree === '270'){
+				this.rebuild(figureType,0,(this.scope.settings.maxColumns * 1) + 1,+1,+1);
+				this.rebuild(figureType,2,(this.scope.settings.maxColumns * -1) - 1,-1,-1);
+				this.rebuild(figureType,3,(this.scope.settings.maxColumns * -2) - 2,-2,-2);
+				this.rebuild(figureType,4,(this.scope.settings.maxColumns * -1) -3,-3,-1);
+			}
+			else {}
+		}
+		else if(figureType === 'l') return;
+		else {}
 	}
 
 	public getNewRVal(currentVal: any): string {
