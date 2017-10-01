93e94e852226502d9281c7fe057f093a7a31c2b0
Anton Makridin <a.i.makridin@yandex.ru>
2017-10-02 00:38:04 +0300

    n/n2 rc ready

--- app/modules/core/f_cr.c.ts
+++ app/modules/core/f_cr.c.ts
@@ -165,19 +165,23 @@ export class f_cr {
 		}
 		else if(type === 'n'){
 			//стартовая позиция, зададим клеткам ид сущности, тип фигуры и номер строки
-			$('.block').eq(0).addClass(`figure_block`).data('type','n').data('row',1).data('column',1).attr('figure',`${hash}`);
-			$('.block').eq(12).addClass(`figure_block`).data('type','n').data('row',2).data('column',1).attr('figure',`${hash}`);
-			$('.block').eq(13).addClass(`figure_block`).data('type','n').data('row',2).data('column',2).attr('figure',`${hash}`);
-			$('.block').eq(14).addClass(`figure_block`).data('type','n').data('row',2).data('column',3).attr('figure',`${hash}`);
-			$('.block').eq(26).addClass(`figure_block`).data('type','n').data('row',3).data('column',3).attr('figure',`${hash}`);		
+			$('.block').eq(4).addClass(`figure_block`).data('type','n').data('row',1).data('column',5).attr('figure',`${hash}`);
+			$('.block').eq(16).addClass(`figure_block`).data('type','n').data('row',2).data('column',5).attr('figure',`${hash}`);
+			$('.block').eq(17).addClass(`figure_block`).data('type','n').data('row',2).data('column',6).attr('figure',`${hash}`);
+			$('.block').eq(18).addClass(`figure_block`).data('type','n').data('row',2).data('column',7).attr('figure',`${hash}`);
+			$('.block').eq(30).addClass(`figure_block`).data('type','n').data('row',3).data('column',7).attr('figure',`${hash}`);
+			$('.block').eq(17).attr('center','true');
+			$('.block').eq(17).attr('limit','0');
 		}
 		else if(type === 'n2'){
 			//стартовая позиция, зададим клеткам ид сущности, тип фигуры и номер строки
-			$('.block').eq(2).addClass(`figure_block`).data('type','n2').data('row',1).data('column',3).attr('figure',`${hash}`);
-			$('.block').eq(12).addClass(`figure_block`).data('type','n2').data('row',2).data('column',1).attr('figure',`${hash}`);
-			$('.block').eq(13).addClass(`figure_block`).data('type','n2').data('row',2).data('column',2).attr('figure',`${hash}`);
-			$('.block').eq(14).addClass(`figure_block`).data('type','n2').data('row',2).data('column',3).attr('figure',`${hash}`);
-			$('.block').eq(24).addClass(`figure_block`).data('type','n2').data('row',3).data('column',1).attr('figure',`${hash}`);		
+			$('.block').eq(6).addClass(`figure_block`).data('type','n2').data('row',1).data('column',7).attr('figure',`${hash}`);
+			$('.block').eq(16).addClass(`figure_block`).data('type','n2').data('row',2).data('column',5).attr('figure',`${hash}`);
+			$('.block').eq(17).addClass(`figure_block`).data('type','n2').data('row',2).data('column',6).attr('figure',`${hash}`);
+			$('.block').eq(18).addClass(`figure_block`).data('type','n2').data('row',2).data('column',7).attr('figure',`${hash}`);
+			$('.block').eq(28).addClass(`figure_block`).data('type','n2').data('row',3).data('column',5).attr('figure',`${hash}`);
+			$('.block').eq(17).attr('center','true');
+			$('.block').eq(17).attr('limit','0');
 		}
 		else {}
 		this.scope.isNewFigure = true;
--- app/modules/core/f_tr.c.ts
+++ app/modules/core/f_tr.c.ts
@@ -950,7 +950,7 @@ export class f_tr {
 				]);
 			}
 			else {}
-		}	
+		}
 		else if(figureType === 'e2'){
 			if(degree === '0'){
 				this.rebuild([
@@ -1026,6 +1026,269 @@ export class f_tr {
 			}
 			else {}
 		}	
+		else if(figureType === 'n'){
+			if(degree === '0'){
+				this.rebuild([
+					{
+						f_type: figureType,
+						id: 0,
+						id_diff: + 2,
+						col_diff: +2,
+						row_diff: 0
+					},
+					{
+						f_type: figureType,
+						id: 1,
+						id_diff: (this.scope.settings.maxColumns * -1) + 1,
+						col_diff: +1,
+						row_diff: -1
+					},
+					{
+						f_type: figureType,
+						id: 3,
+						id_diff: (this.scope.settings.maxColumns * 1) - 1,
+						col_diff: -1,
+						row_diff: +1
+					},
+					{
+						f_type: figureType,
+						id: 4,
+						id_diff: - 2,
+						col_diff: -2,
+						row_diff: 0
+					}
+				]);
+			}
+			else if(degree === '90'){
+				this.rebuild([
+					{
+						f_type: figureType,
+						id: 1,
+						id_diff: (this.scope.settings.maxColumns * 2),
+						col_diff: 0,
+						row_diff: +2
+					},
+					{
+						f_type: figureType,
+						id: 0,
+						id_diff: (this.scope.settings.maxColumns * 1) + 1,
+						col_diff: +1,
+						row_diff: +1
+					},
+					{
+						f_type: figureType,
+						id: 2,
+						id_diff: (this.scope.settings.maxColumns * -2),
+						col_diff: 0,
+						row_diff: -2
+					},
+					{
+						f_type: figureType,
+						id: 3,
+						id_diff: (this.scope.settings.maxColumns * -1) - 1,
+						col_diff: -1,
+						row_diff: -1
+					}
+				]);
+			}
+			else if(degree === '180'){
+				this.rebuild([
+					{
+						f_type: figureType,
+						id: 0,
+						id_diff: + 2,
+						col_diff: +2,
+						row_diff: 0
+					},
+					{
+						f_type: figureType,
+						id: 1,
+						id_diff: (this.scope.settings.maxColumns * -1) + 1,
+						col_diff: +1,
+						row_diff: -1
+					},
+					{
+						f_type: figureType,
+						id: 3,
+						id_diff: (this.scope.settings.maxColumns * 1) - 1,
+						col_diff: -1,
+						row_diff: +1
+					},
+					{
+						f_type: figureType,
+						id: 4,
+						id_diff: - 2,
+						col_diff: -2,
+						row_diff: 0
+					}
+				]);
+			}
+			else if(degree === '270'){
+				this.rebuild([
+					{
+						f_type: figureType,
+						id: 1,
+						id_diff: (this.scope.settings.maxColumns * 2),
+						col_diff: 0,
+						row_diff: +2
+					},
+					{
+						f_type: figureType,
+						id: 0,
+						id_diff: (this.scope.settings.maxColumns * 1) + 1,
+						col_diff: +1,
+						row_diff: +1
+					},
+					{
+						f_type: figureType,
+						id: 2,
+						id_diff: (this.scope.settings.maxColumns * -2),
+						col_diff: 0,
+						row_diff: -2
+					},
+					{
+						f_type: figureType,
+						id: 3,
+						id_diff: (this.scope.settings.maxColumns * -1) - 1,
+						col_diff: -1,
+						row_diff: -1
+					}
+				]);
+			}
+			else {}
+		}	
+		else if(figureType === 'n2'){
+			if(degree === '0'){
+				this.rebuild([
+					{
+						f_type: figureType,
+						id: 0,
+						id_diff: - 2,
+						col_diff: -2,
+						row_diff: 0
+					},
+					{
+						f_type: figureType,
+						id: 1,
+						id_diff: (this.scope.settings.maxColumns * -1) + 1,
+						col_diff: +1,
+						row_diff: -1
+					},
+					{
+						f_type: figureType,
+						id: 3,
+						id_diff: (this.scope.settings.maxColumns * 1) - 1,
+						col_diff: -1,
+						row_diff: +1
+					},
+					{
+						f_type: figureType,
+						id: 3,
+						id_diff: + 2,
+						col_diff: +2,
+						row_diff: 0
+					}
+				]);
+			}
+			else if(degree === '90'){
+				this.rebuild([
+					{
+						f_type: figureType,
+						id: 0,
+						id_diff: (this.scope.settings.maxColumns * 2),
+						col_diff: 0,
+						row_diff: +2
+					},
+					{
+						f_type: figureType,
+						id: 0,
+						id_diff: (this.scope.settings.maxColumns * 1) - 1,
+						col_diff: -1,
+						row_diff: +1
+					},
+					{
+						f_type: figureType,
+						id: 3,
+						id_diff: (this.scope.settings.maxColumns * -1) + 1,
+						col_diff: +1,
+						row_diff: -1
+					},
+					{
+						f_type: figureType,
+						id: 4,
+						id_diff: (this.scope.settings.maxColumns * -2),
+						col_diff: 0,
+						row_diff: -2
+					}
+				]);
+			}
+			else if(degree === '180'){
+				this.rebuild([
+					{
+						f_type: figureType,
+						id: 0,
+						id_diff: - 2,
+						col_diff: -2,
+						row_diff: 0
+					},
+					{
+						f_type: figureType,
+						id: 1,
+						id_diff: (this.scope.settings.maxColumns * -1) + 1,
+						col_diff: +1,
+						row_diff: -1
+					},
+					{
+						f_type: figureType,
+						id: 3,
+						id_diff: (this.scope.settings.maxColumns * 1) - 1,
+						col_diff: -1,
+						row_diff: +1
+					},
+					{
+						f_type: figureType,
+						id: 3,
+						id_diff: + 2,
+						col_diff: +2,
+						row_diff: 0
+					}
+				]);
+			}
+			else if(degree === '270'){
+				this.rebuild([
+					{
+						f_type: figureType,
+						id: 0,
+						id_diff: (this.scope.settings.maxColumns * 2),
+						col_diff: 0,
+						row_diff: +2
+					},
+					{
+						f_type: figureType,
+						id: 0,
+						id_diff: (this.scope.settings.maxColumns * 1) - 1,
+						col_diff: -1,
+						row_diff: +1
+					},
+					{
+						f_type: figureType,
+						id: 3,
+						id_diff: (this.scope.settings.maxColumns * -1) + 1,
+						col_diff: +1,
+						row_diff: -1
+					},
+					{
+						f_type: figureType,
+						id: 4,
+						id_diff: (this.scope.settings.maxColumns * -2),
+						col_diff: 0,
+						row_diff: -2
+					}
+				]);
+			}
+			else {}
+		}
+	
 		else {}
 	}
 
