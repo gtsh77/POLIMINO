e8ac8670446441c54c96e5eec901f0b45a026e33
Anton Makridin <a.i.makridin@yandex.ru>
2017-11-02 09:40:26 +0300

    minor fixes

--- app/modules/core/f_le.p.ts
+++ app/modules/core/f_le.p.ts
@@ -2,9 +2,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({name: 'level'})
 export class f_le implements PipeTransform {
-transform(val: number): string {
+transform(val: number): any {
 	if(val === 0) return '00';
 		if(val < 10) return '0' + val;
-		else return val	;
+		else return val;
 	}
 }
